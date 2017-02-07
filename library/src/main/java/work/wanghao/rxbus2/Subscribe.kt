c48e6784d6ebfaf213a7bef7527ddfab2abaea11
Doublemine <doublemine.w@gmail.com>
2017-02-07 18:06:06 +0800

    update

---
+++ library/src/main/java/work/wanghao/rxbus2/Subscribe.kt
@@ -0,0 +1,12 @@
+package work.wanghao.rxbus2
+
+/**
+ * @author doublemine
+ *         Created  on 2017/02/07 17:54.
+ *         Summary:
+ */
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+@MustBeDocumented
+annotation class Subscribe
